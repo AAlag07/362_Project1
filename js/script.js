74cbffefdbcbcf909095b6ba61340ce4027ae0d3
AAlag07 <112470063+AAlag07@users.noreply.github.com>
2023-03-02 23:15:22 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const generateQuoteOfTheDay = function() {
+    const quotes = [
+        {
+            quote: "Some people are happy campers, me? I have the happy camper van.",
+            author: "John Camper"
+        },
+        {
+            quote: "Baseball is life. This means I have never left my homebase of my basement.",
+            author: "John Baseball"
+        },
+        {
+            quote: "We have to change the weatherman everyday or else we might get it wrong.",
+            author: "John Weather"
+        },
+        {
+            quote: "Road work ahead? It better work.",
+            author: "John Driver"
+        }
+    ];
+        let arrayIndex = Math.floor(Math.random() * quotes.length);
+        document.getElementById("quotes").innerHTML = quotes[arrayIndex].quote;
+        document.getElementById("author").innerHTML = quotes[arrayIndex].author;
+}
+window.onload = function() {
+    generateQuoteOfTheDay;
+    document.getElementById("quote").addEventListener('click', generateQuoteOfTheDay);
+}
